f112cc2eaa948fbf313193aba8274a243391bc84
Nabeel Ali <nabeelali_@outlook.com>
2023-09-18 13:20:39 +0100

    enhanced features to task tracker

--- task-tracker-project/src/App.jsx
+++ task-tracker-project/src/App.jsx
@@ -4,8 +4,8 @@ import './App.css'
 import TaskList from './components/TaskList'
 
 const tasks = [
-  new Task(1, "My Task", "Sample Body", true),
-  new Task(2, "Second Task", "Sample Body 2", false)
+  new Task(1, "My Task", "Sample Body", true, "High", new Date(2023, 8, 28)),
+  new Task(2, "Second Task", "Sample Body 2", false, "Low", new Date(2023, 9, 3))
 ];
 
 function App() {
--- task-tracker-project/src/components/Task.jsx
+++ task-tracker-project/src/components/Task.jsx
@@ -1,8 +1,25 @@
 export default class Task {
-    constructor(taskId, name, body, complete) {
+    constructor(taskId, name, body, complete, priority, dueDate) {
         this.taskId = taskId;
         this.name = name;
         this.body = body;
         this.complete = complete;
+        this.priority = priority;
+        this.dueDate = dueDate;
+    }
+
+    get onTrack() {
+        return new Date() < this.dueDate || this.complete;
+    }
+
+    get formattedDueDate() {
+        const options = {
+            weekday: 'long',
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+        };
+        const formatted = new Intl.DateTimeFormat('en-US', options).format(this.dueDate);
+        return formatted.replace(/,/g, '');
     }
 }
